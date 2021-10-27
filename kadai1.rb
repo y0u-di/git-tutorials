1d8c0cf51c6cd7409f18e27a659e3d27f46d0b29
Shiratsuki <j1815@matsue-ct.ac.jp>
2021-10-27 11:22:48 +0900

    complete kadai1.rb

--- kadai1.rb
+++ kadai1.rb
@@ -1,7 +1,15 @@
-def square(a)
-  a ** 2
+def fizz_buzz(n)
+  if n%15==0
+    'FizzBuzz'
+  elsif n%3==0
+    'Fizz'
+  elsif n%5==0
+    'Buzz'
+  else
+    n
+  end
 end
 
-printf "a = "
-a = gets.chop.to_i
-puts square(a)
+for n in 1..100 do
+  puts fizz_buzz(n)
+end
